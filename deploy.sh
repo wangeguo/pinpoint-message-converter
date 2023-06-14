277c12d451ba23d0b0d7646f8214ef4f53ecc02f
Eguo Wang <wangeguo@gmail.com>
2023-06-14 18:15:23 +0800

    Update release version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kind load docker-image pinpoint-message-converter:1.0.0
+kind load docker-image pinpoint-message-converter:1.2.0
 kubectl delete --ignore-not-found=true -f k8s-dev.yaml
 kubectl apply -f k8s-dev.yaml
